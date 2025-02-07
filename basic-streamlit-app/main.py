3c0c65a9dd96c65c5881b3690906c705d2e65f34
GooboGobbo <dillonswims@hotmail.com>
2025-02-07 11:53:51 -0500

    New File

---
+++ basic-streamlit-app/main.py
@@ -0,0 +1,21 @@
+import streamlit as st
+import pandas as pd
+
+st.title("Palmer's Penguins")
+st.subheader("Made by James Edge")
+
+
+st.write("This is an interactive app where you can manipulate data in Palmer's Penguins data set")
+
+df = pd.read_csv("basic-streamlit-app/data/penguins.csv")
+
+species = st.selectbox('Select a Species', df["species"].unique())
+st.write(f'Penguins in {species}')
+st.dataframe(df[df['species'] == species])
+
+bodymass = st.slider("Choose a maximum body mass",
+          min_value = df['body_mass_g'].min(),
+          max_value = df['body_mass_g'].max())
+
+st.write(f'Salaries under {bodymass}')
+st.dataframe(df[df['body_mass_g'] <= bodymass])
