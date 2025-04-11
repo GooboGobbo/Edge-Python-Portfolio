e33335d8032a1c2c6eb4809a1da3fc410193d265
GooboGobbo <dillonswims@hotmail.com>
2025-04-11 14:11:07 -0400

    Update app.py

--- NERStreamlitApp/app.py
+++ NERStreamlitApp/app.py
@@ -35,25 +35,7 @@ st.markdown(
     """,
     unsafe_allow_html=True
 )
-st.markdown(
-    """
-    <style>
-    /* Make Streamlit buttons gold and clean */
-    div.stButton > button:first-child {
-        background-color: #C99700;
-        color: black;
-        font-weight: 600;
-        border-radius: 5px;
-        border: none;
-    }
-    div.stButton > button:first-child:hover {
-        background-color: #b58900;
-        color: white;
-    }
-    </style>
-    """,
-    unsafe_allow_html=True
-)
+
 
 # =============================================================================
 # SIDEBAR CONTENT
